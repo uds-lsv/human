43f3dd5f30108f3e04026121e6853dd6eb24fe1f
Moritz Wolf <Moritz.Wolf@dfki.de>
2021-12-14 13:52:21 +0100

    update node dependencies ts compile target set to es6

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,11 @@ const postcss = require('gulp-postcss')
 const cssnano = require('cssnano')
 // const autoprefixer = require('autoprefixer')
 // const concat = require('gulp-concat')
-const uglify = require('gulp-uglify')
+const uglify = require('gulp-terser')
 const buffer = require('vinyl-buffer')
 const sourcemaps = require('gulp-sourcemaps')
 
-var minifycss = function() {
+const minifycss = function () {
     const plugins = [
         cssnano(),
         // autoprefixer()
@@ -23,42 +23,66 @@ var minifycss = function() {
         .pipe(dest('app/static/dist'))
 }
 
-var watch = function() {
-    return watchBuild
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('sourcemap/'))
-        .pipe(dest('app/static/dist/'))
+const watch = function () {
+    return (
+        watchBuild
+            .bundle()
+            .pipe(source('bundle.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({ loadMaps: true }))
+            // .pipe(uglify())
+            .pipe(sourcemaps.write('sourcemap/'))
+            .pipe(dest('app/static/dist/'))
+    )
 }
 
-var watchBuild = browserify({
+const watchBuild = browserify({
     basedir: '.',
     debug: true,
-    entries: [ 'app/static/start.ts' ],
+    entries: ['app/static/start.ts'],
     cache: {},
     packageCache: {},
-    plugin: [ watchify ],
+    plugin: [watchify],
 }).plugin(tsify)
 
-var defaultBuild = function() {
-    return browserify({
-        basedir: '.',
-        debug: true,
-        entries: [ 'app/static/start.ts' ],
-        cache: {},
-        packageCache: {},
-    })
-        .plugin(tsify)
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('sourcemap/'))
-        .pipe(dest('app/static/dist/'))
+const defaultBuild = function () {
+    return (
+        browserify({
+            basedir: '.',
+            debug: true,
+            entries: ['app/static/start.ts'],
+            cache: {},
+            packageCache: {},
+        })
+            .plugin(tsify)
+            .bundle()
+            .pipe(source('bundle.js'))
+            .pipe(buffer())
+            // .pipe(uglify())
+            .pipe(sourcemaps.init({ loadMaps: true }))
+            .pipe(sourcemaps.write('sourcemap/'))
+            .pipe(dest('app/static/dist/'))
+    )
+}
+
+const productionBuild = function () {
+    return (
+        browserify({
+            basedir: '.',
+            debug: false,
+            entries: ['app/static/start.ts'],
+            cache: {},
+            packageCache: {},
+        })
+            .plugin(tsify)
+            .bundle()
+            .pipe(source('bundle.js'))
+            .pipe(buffer())
+            .pipe(uglify())
+            // .pipe(sourcemaps.init({ loadMaps: true }))
+            // .pipe(sourcemaps.write('sourcemap/'))
+            .pipe(dest('app/static/dist/'))
+    )
 }
 
 watchBuild.on('update', watch)
@@ -70,3 +94,5 @@ exports.build = defaultBuild
 
 exports.watch = series(minifycss, watch)
 exports.minifycss = minifycss
+
+exports.production = parallel(productionBuild, minifycss)
